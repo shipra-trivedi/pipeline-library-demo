d4f344ff4d2efb54327a99ce6b453459f2688325
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-04 20:42:51 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,14 +1,9 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  def filePath = readFile "Hello.csv"                   
-	def lines = filePath.readLines()
-	 def linesbyline = filePath.readLines() 
-      
-	for (line in linesbyline) {                                            
-                      
-                        println "$line"
-                        }  
+  new File("Hello.csv").eachLine { line ->
+    println line
+}
 
   echo "Hello, ${name}."
 }
