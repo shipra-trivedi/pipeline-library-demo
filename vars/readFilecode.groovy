833833a877d4450c7c961d4604eead82550ad21f
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-05 12:04:58 +0530

    Update readFilecode.groovy

--- vars/readFilecode.groovy
+++ vars/readFilecode.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String filepath ) {
-  new File("https://github.com/KirtiPhirke98/pipeline-library-demo/blob/master/vars/Hello.csv").eachLine { line ->
+  new File(filepath).eachLine { line ->
     println line
 }
   
