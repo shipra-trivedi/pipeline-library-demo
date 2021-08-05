6be5640cca4c54654d73d1f9575cd851692223c1
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-05 11:28:16 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  new File("${name}").eachLine { line ->
-    println line
-}
-
-  echo "Hello, ${name}."
+    echo "Hello, ${name}."
 }
 
