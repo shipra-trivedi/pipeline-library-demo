06b3ac5d5747f55cd76bc8dc0474631aeac963df
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-05 15:01:40 +0530

    Update readFilecode.groovy

--- vars/readFilecode.groovy
+++ vars/readFilecode.groovy
@@ -1,9 +1,15 @@
 #!/usr/bin/env groovy
 
 def call(String filepath ) {
-  new File(filepath).eachLine { line ->
-    println line
-}
+   
+  //def filePath = readFile "Hello.csv"                   
+	def lines = filePath.readLines()
+	 def linesbyline = filePath.readLines() 
+      
+	for (line in linesbyline) {                                            
+                      
+                        println "$line"
+                        }  
   
  
    
