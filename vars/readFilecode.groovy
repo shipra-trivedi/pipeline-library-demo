b5bdd1dbf201afc06e62a5ca5f2fcc939a231f3c
Kirti Phirke <54802757+KirtiPhirke98@users.noreply.github.com>
2021-08-05 14:21:34 +0530

    Update readFilecode.groovy

--- vars/readFilecode.groovy
+++ vars/readFilecode.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String filepath ) {
-  new File("output.csv") << new URL (filepath).getText()
+  new File("output.csv") << new URL ("${filepath}").getText()
   new File("output.csv").eachLine { line ->
     println line
 }
